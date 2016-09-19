2e3282b6967d067759532fdd83a0399caed29fa3
abwhitley <a.blakewhitley@gmail.com>
2016-09-18 21:41:45 -0400

    finished chapter 11

--- HomePwner/ItemStore.m
+++ HomePwner/ItemStore.m
@@ -19,9 +19,7 @@
     self = [super init];
     if (self) {
         _items = [NSMutableArray array];
-        for (int i = 0; i < 5; i++) {
-            [self createItem];
-        }
+
     }
     return self;
 }
@@ -34,5 +32,21 @@
     [self.items addObject:newItem];
     return newItem;
 }
+- (void)removeItem:(Item *)item {
+    [self.items removeObject:item];
+}
+
+- (void)moveItemAtIndex:(NSUInteger)oldIndex
+                toIndex:(NSUInteger)newIndex {
+    if (oldIndex == newIndex) {
+        return; }
+    // Get a reference to the object being removed so it can be re-inserted
+    Item *item = self.items[oldIndex];
+    // Remove it from the array
+    [self.items removeObjectAtIndex:oldIndex];
+    // Insert it at the new index
+    [self.items insertObject:item atIndex:newIndex];
+}
+
 
 @end
--- ItemsViewController.m
+++ ItemsViewController.m
@@ -14,6 +14,31 @@
 @end
 
 @implementation ItemsViewController
+
+//MARK: - Actions
+-(IBAction)addNewItem:(id)sender{
+    // Create a new item and add it to the store
+    Item *newItem = [self.itemStore createItem];
+    // Figure out where that item is in the array
+    NSUInteger itemIndex = [self.itemStore.allItems indexOfObject:newItem];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:itemIndex inSection:0];    // Insert this row into the table
+    [self.tableView insertRowsAtIndexPaths:@[indexPath]
+                          withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+-(IBAction)toggleEditingMode:(id)sender {
+    //if you are currently in edditing mode
+    if(self.editing){
+        //change the text of the button to inform the user
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        //turn off editiing mode
+        [self setEditing:NO animated:YES];
+    }else{
+        //Change the text of the button to inform the user
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        //turn off editing mode
+        [self setEditing:YES animated:YES];
+    }
+}
 // MARK: - View Lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -44,5 +69,46 @@
     cell.detailTextLabel.text = [NSString stringWithFormat:@"$%d", item.valueInDollars];
     return cell;
 }
+- (void)tableView:(UITableView *)tableView
+commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+forRowAtIndexPath:(NSIndexPath *)indexPath {
+    // If the table view is asking to commit a delete command...
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        Item *item = self.itemStore.allItems[indexPath.row];
+        
+        NSString *title = [NSString stringWithFormat:@"Delete %@?", item.name];
+        NSString *message = @"Are you sure that you want to delete this item?";
+        UIAlertController *ac =[UIAlertController alertControllerWithTitle:title
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleActionSheet];
+        UIAlertAction *cancelAction =
+        [UIAlertAction actionWithTitle:@"Cancel"
+                                 style:UIAlertActionStyleCancel
+                               handler:nil];
+        UIAlertAction *deleteAction =
+        [UIAlertAction actionWithTitle:@"Delete"
+                                 style:UIAlertActionStyleDestructive
+                               handler:^(UIAlertAction * _Nonnull action) {
+                                   // Remove the item from the store
+                                   [self.itemStore removeItem:item];
+                                   // Also remove its cell from the table view
+                                   [self.tableView deleteRowsAtIndexPaths:@[indexPath]
+                                                         withRowAnimation:UITableViewRowAnimationAutomatic];
+                               }];
+        [ac addAction:cancelAction];
+        [ac addAction:deleteAction];
+        // Present the View Controller
+        [self presentViewController:ac animated:YES completion:nil];
+        
+         }
+}
+- (void)tableView:(UITableView *)tableView
+moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+      toIndexPath:(NSIndexPath *)destinationIndexPath {
+    [self.itemStore moveItemAtIndex:sourceIndexPath.row
+                            toIndex:destinationIndexPath.row];
+}
+
+         
 
 @end
